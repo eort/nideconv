172911eaffe501a2105b883405dcf47be0f36d84
eort <e.ort@vu.nl>
2017-10-03 14:49:07 +0200

    bakcup

--- prepareGLM_lvl3.py
+++ prepareGLM_lvl3.py
@@ -32,52 +32,30 @@ def run(cfg):
     fsfDir = op.join(baseDir,cfg['fsfDir']%ID) # Dir of newly created fsf files
     infoDir = op.join(baseDir,cfg['infoDir']) # Dir of newly created fsf files
     modelDir = op.join(baseDir,cfg['modelDir']%s) # Dir for newly created submit files
+    template = op.join(baseDir,cfg['templateDir'],cfg['templateFSF']) # template fsf file
     templateSubmit = op.join(baseDir,cfg['templateDir'],cfg['templateSubmit']) # template submit file
-    shell()
+
     if not op.exists(fsfDir): # submit dir
        print('Creating new folder %s'%(fsfDir))
        os.makedirs(fsfDir)
     """""""""""""""""""""""""""
     #STEP 2: CREATE FSF and SUBFILES
     """""""""""""""""""""""""""
-    for SUB in subjects:
-        # First skip bad subjects
-        if SUB in cfg['excl_subj']:
-            continue
-        # create output directory if not yet existent
-        if not op.exists(modelDir%(SUB)): # submit dir
-            print('Creating new folder %s'%(modelDir%(SUB)))
-            os.system("mkdir %s"%(modelDir%(SUB)))
-        if not op.exists(fsfDir%(SUB,ID)): # submit dir
-            print('Creating new folder %s'%(fsfDir%(SUB,ID)))
-            os.system("mkdir %s"%(fsfDir%(SUB,ID)))
+    for COPE in range(nCopes+1):
         # copy submit template to subject specific dir
-        submitfile = op.join(modelDir%SUB,'sub-%02d_%s_2ndlvl.submit'%(SUB,ID))
-        os.system('sed -e "s/##SUB##/%02d/g" < %s > %s'%(SUB,templateSubmit,submitfile))
-              
-        # loop over runs and create fsf files
-        for COPE in range(1,nCopes+1):
-            goodRuns = range(1,runsPerSubject[SUB]+1)
-            badRuns = emptyRuns['%02d'%SUB]
-            requiredEV = copeDepend['%s'%COPE]
-            for r in badRuns:
-                if r[1] in requiredEV:
-                    try:
-                        goodRuns.remove(int(r[0]))
-                    except:
-                        print 'RUN %s already removed'%r
-            
-            template = op.join(baseDir,cfg['templateDir'],cfg['templateFSF_%s'%len(goodRuns)]) # template fsf file
-            # define the output fsf filename
-            outfile = op.join(fsfDir%(SUB,ID),'sub-%02d_cope-%02d_%s.fsf'%(SUB,COPE,ID))
-            # make fsf files
-            os.system('sed -e "s/##ID##/%s/g;s/##SUB##/%02d/g; s/##COPE##/%d/g" < %s > %s'%(ID,SUB,COPE,template,outfile))
-            for idx,RUN in enumerate(goodRuns,1):
-                os.system('sed -i "s/##RUN%s##/%02d/g" %s'%(idx,RUN,outfile))
-            # add fsf file to submit file
-            with open(submitfile, 'a') as out:
-                out.write("\narguments = fsf/2ndlvl/%s/sub-%02d_cope-%02d_%s.fsf\n"%(ID,SUB,COPE,ID))
-                out.write("queue")
+        submitfile = op.join(fsfDir,'3rdlvl_%s.submit'%ID)
+        os.system('cp %s %s'%(templateSubmit,submitfile))
+        shell()             
+        # define the output fsf filename
+        outfile = op.join(fsfDir,'3rdlvl_%s.fsf'%(ID)
+        # make fsf files
+        os.system('sed -e "s/##ID##/%s/g;s/##SUB##/%02d/g; s/##COPE##/%d/g" < %s > %s'%(ID,SUB,COPE,template,outfile))
+        for idx,RUN in enumerate(goodRuns,1):
+            os.system('sed -i "s/##RUN%s##/%02d/g" %s'%(idx,RUN,outfile))
+        # add fsf file to submit file
+        with open(submitfile, 'a') as out:
+            out.write("\narguments = fsf/2ndlvl/%s/sub-%02d_cope-%02d_%s.fsf\n"%(ID,SUB,COPE,ID))
+            out.write("queue")
         # if wished submit jobs to condor
         if cfg['execute']:
             os.system("condor_submit %s"%submitfile)
