1fedf407e5f829f151e1e8114c53669c5b314293
eort <e.ort@vu.nl>
2017-10-11 17:57:23 +0200

    updateOct11

--- makeSummaryFile.py
+++ makeSummaryFile.py
@@ -7,7 +7,14 @@ import glob
 import local2remote as l2r
 
 
-def run(key,outfile,conv= True):
+def run(key,outfile,mode,conv= True ):
+	"""
+	key:		Which Analysis directory (keyword, e.g. searchF)
+	outfile: 	Which HTML ouput file to use
+	mode: 		What kind of information to include
+				1:DM,2:MC,3:Reg,4:DM,MC,5:DM,REG,6:REG,MC,7:All
+	conv:		Want to convert the file to locale? 1,0
+	"""
 	all_feats = glob.glob('/home/data/exppsy/ora_Amsterdam/sub*/models/1stlvl/*%s.feat/'%key)
 	all_feats.sort()
 
@@ -15,20 +22,23 @@ def run(key,outfile,conv= True):
 	    for f in all_feats:
 			out.write("<p>============NEXT RUN==================<br>")
 			out.write("<p>%s <br>"%(f))
-			out.write("<p>____________DESIGN MATRICES__________<br>")
-			out.write("<IMG SRC=\"%s/design.png\"> <br>"%f)
-			out.write("<IMG SRC=\"%s/design_cov.png\" > <br>"%f)
-			out.write("<p>________________MOTION_____________<br>")
-			out.write("<IMG SRC=\"%s/mc/disp.png\"> <br>"%f)
-			out.write("<IMG SRC=\"%s/mc/rot.png\"> <br>"%f)
-			out.write("<IMG SRC=\"%s/mc/trans.png\" > <br>"%f)
-			out.write("<p>____________Registration__________<br>")
-			out.write("<p>____________example_func2highres__________<br>")
-			out.write("<p><IMG SRC=\"%s/reg/example_func2highres.png\" WIDTH=1200> <br>"%f)
-			out.write("<p>____________example_func2standard__________<br>")
-			out.write("<p><IMG SRC=\"%s/reg/example_func2standard.png\" WIDTH=1200> <br>"%f)
-			out.write("<p>____________highres2standard__________<br>")			
-			out.write("<p><IMG SRC=\"%s/reg/highres2standard.png\" WIDTH=1200> <br>"%f)
+			if mode in [1,4,5,7]:
+				out.write("<p>~~~~~~~~~~~~~DESIGN MATRICES~~~~~~~~~~<br>")
+				out.write("<IMG SRC=\"%s/design.png\"> <br>"%f)
+				out.write("<IMG SRC=\"%s/design_cov.png\" > <br>"%f)
+			if mode in [2,4,6,7]:
+				out.write("<p>~~~~~~~~~~~~~MOTION~~~~~~~~~~<br>")
+				out.write("<IMG SRC=\"%s/mc/disp.png\"> <br>"%f)
+				out.write("<IMG SRC=\"%s/mc/rot.png\"> <br>"%f)
+				out.write("<IMG SRC=\"%s/mc/trans.png\" > <br>"%f)		
+			if mode in [3,5,6,7]:
+				out.write("<p>~~~~~~~~~~~REGISTRATION~~~~~~~~~~~~<br>")
+				out.write("<p>~~~~~~~~~~~example_func2highres~~~~~~~~~~~~<br>")
+				out.write("<p><IMG SRC=\"%s/reg/example_func2highres.png\" WIDTH=1200> <br>"%f)
+				out.write("<p>~~~~~~~~~~~example_func2standard~~~~~~~~~~~~<br>")
+				out.write("<p><IMG SRC=\"%s/reg/example_func2standard.png\" WIDTH=1200> <br>"%f)
+				out.write("<p>~~~~~~~~~~~highres2standard~~~~~~~~~~~~<br>")			
+				out.write("<p><IMG SRC=\"%s/reg/highres2standard.png\" WIDTH=1200> <br>"%f)
 	if conv:
 		infile, extension = op.splitext(outfile)
 		outputfile = infile + '_loc' + extension
@@ -43,12 +53,19 @@ if __name__ == '__main__':
 	try:
 		outfile = op.abspath(sys.argv[2])
 	except IndexError as e:
-		outfile = '/home/data/exppsy/ora_Amsterdam/generalInfo/QA_lvl1_%s.html'%featkey
+		outfile = '/home/data/exppsy/ora_Amsterdam/QA/QA_lvl1_%s.html'%featkey
 	try:
-		conv = int(sys.argv[3])
+		mode = int(sys.argv[3])
+		try: assert mode in range(1,8)
+		except AssertionError as e: print('Invalid conversion parameter, will do conversion anyway');conv =1
+	except IndexError as e:
+		mode = 7
+
+	try:
+		conv = int(sys.argv[4])
 		try: assert conv in [0,1]
 		except AssertionError as e: print('Invalid conversion parameter, will do conversion anyway');conv =1
 	except IndexError as e:
 		conv = 1
 
-	run(featkey,outfile, conv = conv)
+	run(featkey,outfile, mode = mode,conv = conv)
