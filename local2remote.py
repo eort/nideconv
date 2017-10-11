959ed364a67aa96ff885d16725d0154d88f2c274
eort <e.ort@vu.nl>
2017-10-11 14:07:15 +0200

    new

--- local2remote.py
+++ local2remote.py
@@ -13,32 +13,35 @@ from IPython import embed as shell
 import subprocess
 
 
-def run(inputfile,outputfile,mode = rem2loc):
-    pass
-
+def run(inputfile,outputfile,mode = 'rem2loc'):
+    
+    if mode == 'rem2loc':
+        key = '/home/data/exppsy/ora_Amsterdam'
+        repl = '/run/user/1000/gvfs/sftp:host=medusa.ovgu.de,user=ort/home/data/exppsy/ora_Amsterdam/'
+    elif mode == 'loc2rem':
+        repl = '/home/data/exppsy/ora_Amsterdam'
+        key = '/run/user/1000/gvfs/sftp:host=medusa.ovgu.de,user=ort/home/data/exppsy/ora_Amsterdam/'
+        
+    with open(inputfile,'r') as infile:
+        for x,l in enumerate(infile):
+            shell()
+            if key in l
 if __name__ == '__main__':
-    
+    # Check input
     try:
-    	inputfile = op.abspath(sys.argv[1])    
-    
-    
-    
-        try:
-        jsonfile = sys.argv[1]
+        	inputfile = op.abspath(sys.argv[1]) 
     except IndexError:
         print('You need to specify configuration file for this analysis')
-        sys.exit()
+        sys.exit()        
     try:
-        with open(jsonfile) as data_file:    
-            cfg = json.load(data_file)
-    except IOError as e:
-        print "The provided file does not exist. Either put a default .json file "\
-        "in the directory of this script, or provide a valid file in the command line."
-        sys.exit()
+        	outputfile = op.abspath(sys.argv[2]) 
+    except IndexError:
+        infile, extension = op.splitext(inputfile)
+        outputfile = op.join(infile,'_conv',extension)
+        print('Use default outputfile ')              
+    try:
+        	mode = sys.argv[3]
+    except IndexError:
+        mode = 'rem2loc'
+        print('Convert to locale (default)')        
     run(inputfile,outputfile,mode)
-try:
-	inputfile = op.abspath(sys.argv[1])
-except IndexError as e:
-    parentDir = '/home/data/exppsy/ora_Amsterdam'
-    #parentDir = '/run/user/1000/gvfs/sftp:host=medusa.ovgu.de,user=ort/home/data/exppsy/ora_Amsterdam/'
-    print 'Use default path %s'%parentDir
