4a142289b34a2afab173e9ead3ca51ba19cd9033
eort <e.ort@vu.nl>
2017-10-11 12:30:24 +0200

    update motion

--- motionScrubbing.py
+++ motionScrubbing.py
@@ -23,7 +23,6 @@ out_bad_pp = op.join(parentDir,'generalInfo/exclSubsMotion.html')
 os.system("rm %s"%outhtml )
 os.system("rm %s"%out_bad_pp )
 
-shell()
 
 for bold in boldfiles:
     strippedBold = bold[:-7] # remove file path
@@ -33,7 +32,7 @@ for bold in boldfiles:
         print('Creating new folder %s'%motionDir)
         os.system("mkdir %s"%motionDir)
     
-    os.system('fsl_motion_outliers -i -o %s/confound.txt --fd --thresh=0.9 -p %s/fd_plot -v > %s/outlier_output.txt'%(strippedBold,motionDir,motionDir,motionDir))
+    os.system('fsl5.0-fsl_motion_outliers -i -o %s/confound.txt --fd --thresh=0.9 -p %s/fd_plot -v > %s/outlier_output.txt'%(strippedBold,motionDir,motionDir,motionDir))
     os.system("cat %s/outlier_output.txt >> %s"%(motionDir, outhtml))
     os.system("echo '<p>=============<p>FD plot %s <br><IMG BORDER=0 SRC=%s/fd_plot.png WIDTH=100%s></BODY></HTML>' >> %s"%(motionDir, motionDir,'%', outhtml))
     if os.path.isfile("%s/confound.txt"%motionDir)==False:
@@ -43,18 +42,6 @@ for bold in boldfiles:
     num_scrub = [int(s) for s in output.split() if s.isdigit()]
     shell()
     if num_scrub[0]>45:
-        with open(out_bad_bold_list, "a") as myfile:
-          myfile.write("%s\n"%(cur_bold))      
+        with open(out_bad_pp, "a") as myfile:
+          myfile.write("%s\n"%motionDir)      
     
-with open(outfile, 'w') as out:    
-    for f in all_feats:
-		out.write("<p>============================================")
-		out.write("<p>%s"%(f))
-		out.write("<IMG SRC=\"%s/design.png\">"%f)
-		out.write("<IMG SRC=\"%s/design_cov.png\" >"%f)
-		out.write("<IMG SRC=\"%s/disp.png\">"%f)
-		out.write("<IMG SRC=\"%s/rot.png\">"%f)
-		out.write("<IMG SRC=\"%s/trans.png\" >"%f)
-		out.write("<p><IMG SRC=\"%s/example_func2highres.png\" WIDTH=1200>"%f)
-		out.write("<p><IMG SRC=\"%s/example_func2standard.png\" WIDTH=1200>"%f)
-		out.write("<p><IMG SRC=\"%s/highres2standard.png\" WIDTH=1200>"%f)
