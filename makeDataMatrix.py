72af76b16c30b38e340e0b08c9eade4a2a1aab1c
eort <e.ort@vu.nl>
2018-03-27 17:11:45 +0200

    deletions

--- makeDataMatrix.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-
-dm = np.zeros((80,10)) # make a data matrix
-for i in range(10):                             
-	j = i*8; dm[j:j+8,i] = 1 # fill matrix with 1s (group average)
-np.savetxt('datamatrix.txt',dm,delimiter='\t',fmt='%d')
